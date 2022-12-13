f23cc1b3145184763569923bc5c2fa348ca5a84c
Oguzhan Ulusoy <oguzhan.ulusoy@orioninc.com>
2022-12-13 21:50:01 +0300

    implemented get_user_by_email and get_user_by_pk

--- user/core/urls.py
+++ user/core/urls.py
@@ -6,4 +6,6 @@ VIEW_HOME = "view/"
 
 urlpatterns = [
     path(API_HOME + "get_user_list", views.get_user_list, name='get_user_list'),
+    path(API_HOME + "get_user_by_email/<str:email>", views.get_user_by_email, name='get_user_by_email'),
+    path(API_HOME + "get_user_by_pk/<str:pk>", views.get_user_by_pk, name= 'get_user_by_pk'),
 ]
--- user/core/views.py
+++ user/core/views.py
@@ -7,7 +7,7 @@ from http import HTTPStatus
 
 is_log = True
 response = 'response'
-not_found = 'not found'
+no_context = 'not found'
 
 
 def get_user_list(request):
@@ -21,14 +21,62 @@ def get_user_list(request):
             if is_log:
                 logging.info("Retrieved all users from database.")
         except Exception as e:
-            context[response] = not_found
+            context[response] = no_context
             status = HTTPStatus.NOT_FOUND
             if is_log:
                 logging.error(e)
         return JsonResponse(context, status=status)
     else:
-        context[response] = not_found
+        context[response] = no_context
         status = HTTPStatus.BAD_REQUEST
         if is_log:
             logging.error("Invalid get request")
+        return JsonResponse(context, status=status)
 
+
+def get_user_by_email(request, email):
+    context = {}
+    status = 0
+    if request.method == 'GET':
+        try:
+            user = User.objects.get(email=email)
+            context = {response: user.serialize()}
+            status = HTTPStatus.ACCEPTED
+            if is_log:
+                logging.info("Retrieved user accordance with e-mail from database.")
+        except Exception as e:
+            context[response] = no_context
+            status = HTTPStatus.NOT_FOUND
+            if is_log:
+                logging.error(e)
+        return JsonResponse(context, status=status)
+    else:
+        context[response] = no_context
+        status = HTTPStatus.BAD_REQUEST
+        if is_log:
+            logging.error("Invalid get request")
+        return JsonResponse(context, status=status)
+
+
+def get_user_by_pk(request, pk):
+    context = {}
+    status = 0
+    if request.method == 'GET':
+        try:
+            user = User.objects.get(pk=pk)
+            context = {response: user.serialize()}
+            status = HTTPStatus.ACCEPTED
+            if is_log:
+                logging.info("Retrieved user accordance with pk from database.")
+        except Exception as e:
+            context[response] = no_context
+            status = HTTPStatus.NOT_FOUND
+            if is_log:
+                logging.error(e)
+        return JsonResponse(context, status=status)
+    else:
+        context[response] = no_context
+        status = HTTPStatus.BAD_REQUEST
+        if is_log:
+            logging.error("Invalid get request")
+        return JsonResponse(context, status=status)
