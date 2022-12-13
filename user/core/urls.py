464216d28851b6f4fe1cc6c8c97f98e478eb6334
Oguzhan Ulusoy <oguzhan.ulusoy@orioninc.com>
2022-12-13 21:50:21 +0300

    fixed typo

--- user/core/urls.py
+++ user/core/urls.py
@@ -7,5 +7,5 @@ VIEW_HOME = "view/"
 urlpatterns = [
     path(API_HOME + "get_user_list", views.get_user_list, name='get_user_list'),
     path(API_HOME + "get_user_by_email/<str:email>", views.get_user_by_email, name='get_user_by_email'),
-    path(API_HOME + "get_user_by_pk/<str:pk>", views.get_user_by_pk, name= 'get_user_by_pk'),
+    path(API_HOME + "get_user_by_pk/<str:pk>", views.get_user_by_pk, name='get_user_by_pk'),
 ]
